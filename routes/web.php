3fb8ebb116ad5e747f21e24deedb7321beac0115
Valdir Júnior <valdirmsjunior@gmail.com>
2023-08-02 21:02:30 -0300

    HEY-9: adding the option to auto login when isLocal()

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,13 @@ use App\Http\Controllers\{ProfileController, QuestionController};
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+
+    if(app()->isLocal()) {
+        auth()->loginUsingId(1);
+
+        return to_route('dashboard');
+    }
+
     return view('welcome');
 });
 
