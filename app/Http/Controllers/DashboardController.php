325622124b80f767b74881f568102a193de23aa7
Valdir Júnior <valdirmsjunior@gmail.com>
2023-08-23 20:06:25 -0300

    HEY-11: evitando o lazyload na consulta

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -13,7 +13,9 @@ class DashboardController extends Controller
     public function __invoke(): View
     {
         return view('dashboard', [
-            'questions' => Question::all(),
+            'questions' => Question::withSum('votes', 'like')
+                ->withSum('votes', 'unlike')
+                ->get(),
         ]);
     }
 }
--- resources/views/components/question.blade.php
+++ resources/views/components/question.blade.php
@@ -10,7 +10,7 @@
             <button class="flex items-start space-x-1 text-green-500">
                 <x-icons.thumbs-up class="w-5 h-5 text-green-500 cursor-pointer hover:text-green-300"/>
                 
-                <span>{{ $question->likes}}</span>
+                <span>{{ $question->votes_sum_like ?: 0}}</span>
             </button>
         </x-form>
         
@@ -18,7 +18,7 @@
             <button class="flex items-start space-x-1 text-green-500">
                 <x-icons.thumbs-down class="w-5 h-5 text-red-500 cursor-pointer hover:text-red-300"/>
                 
-                <span>{{ $question->unlikes}}</span>
+                <span>{{ $question->votes_sum_unlike ? : 0}}</span>
             </button>
         </x-form>
         
